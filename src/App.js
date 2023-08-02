5c43d73c4b8de69fb8a973965d900e61fc12558c
nikita <nikita.parekh@unboxrobotics.com>
2023-08-02 14:29:31 +0530

    socket

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import socketIOClient from "socket.io-client";
+
+const socket = socketIOClient(
+  "http://192.168.11.241:4010/session-controller/fleet_events"
+);
+
+socket.on("connect", (data) => {
+  console.log("fleet_events connected");
+});
+console.log(socket);
+socket.on("feeders_event", (data) => {
+  console.log("feeders_event", data);
+});
+socket.on("robots_event", (data) => {
+  console.log("robots_event", data);
+});
+socket.on("connect_error", (error) => {
+  console.error("Socket connection error:", error);
+});
 
 function App() {
   return (
